b50d86d46aa55c78e608824cee9d768c782deb99
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-10 15:11:07 +0300

    Boolean added

---
+++ Example006_Boolean/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Horray! That's Maria");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
