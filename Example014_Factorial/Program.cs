c8749530f2cc420fe1d13a3a57065b47277079b3
aleksei <aleksei.bazhaikin@gmail.com>
2023-09-28 15:06:10 +0300

    Day 4 added

---
+++ Example014_Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 20; i++)
+// {
+//     Console.WriteLine(Factorial(i));
+// }
+
+
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 20; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
